6c494e9ccae9c923ca98a89a225e9d578cbc5c16
KAMB02 <kallouan25@gmail.com>
2025-03-24 00:02:43 +0000

    projet d'archivre

---
+++ donnee.js
@@ -0,0 +1,17 @@
+// Fonction pour le formulaire "Cours"
+function navigateToCours() {
+    const selectElement = document.getElementById("options");
+    const selectedValue = selectElement.value;
+    if (selectedValue) {
+        window.location.href = selectedValue;
+    }
+}
+
+// Fonction pour le formulaire "TD & Examens corrigés"
+function navigateToExos() {
+    const selectElement = document.getElementById("exos");
+    const selectedValue = selectElement.value;
+    if (selectedValue) {
+        window.location.href = selectedValue;
+    }
+}
