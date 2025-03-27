12277b9f139116e7da28a1b064e4d58a358dfeea
KADJO ALLOUAN MOISE BIENVENUE <kallouan25@gmail.com>
2025-03-27 23:14:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-// Fonction pour le formulaire "Cours"
-function navigateToCours() {
-    const selectElement = document.getElementById("options");
-    const selectedValue = selectElement.value;
-    if (selectedValue) {
-        window.location.href = selectedValue;
+function loadContent(niveau) {
+    const contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = `
+        <h3>${niveau}</h3>
+        <p>Voici les cours et exercices corrigés pour le niveau ${niveau}. Sélectionnez un sujet pour plus de détails.</p>
+    `;
+}
+
+
+function loadContent(niveau) {
+    const contentDiv = document.getElementById('content');
+
+    if (niveau === 'Licence 1') {
+        contentDiv.innerHTML = `
+            <h3>Cours et Exercices - Licence 1</h3>
+            <p>Voici les cours disponibles pour le niveau Licence 1 :</p>
+            <ul>
+                <li>Introduction à la programmation</li>
+                <li>Algèbre et calcul matriciel</li>
+                <li>Physique générale</li>
+            </ul>
+            <p>Exercices corrigés disponibles :</p>
+            <ul>
+                <li>Exercice 1 - Structures conditionnelles</li>
+                <li>Exercice 2 - Calcul matriciel</li>
+            </ul>
+        `;
+    } 
+    else if(niveau === 'Licence1')
+     {
+        contentDiv.innerHTML = `
+            <h3>${niveau}</h3>
+            <p>Voici les pour le niveau ${niveau}. Sélectionnez un sujet pour plus de détails.</p>
+        `;
+    }
+
+    else {
+        contentDiv.innerHTML = `
+            <h3>${niveau}</h3>
+            <p>Voici les cours et exercices corrigés pour le niveau ${niveau}. Sélectionnez un sujet pour plus de détails.</p>
+        `;
     }
 }
 
-// Fonction pour le formulaire "TD & Examens corrigés"
-function navigateToExos() {
-    const selectElement = document.getElementById("exos");
-    const selectedValue = selectElement.value;
-    if (selectedValue) {
-        window.location.href = selectedValue;
-        print(salut)
-    }
-}
